f5736754aeb237d0ba1e657e022b1748dda9e762
brightyoyo <goodev@goodev.local>
2016-02-19 12:28:20 +0800

    删除refresh代码，添加ImageLoader

--- app/src/main/java/io/liamju/comm/sample/interfaces/OnItemListClickListener.java
+++
@@ -1,7 +0,0 @@
-package io.liamju.comm.sample.interfaces;
-
-import android.view.View;
-
-public interface OnItemListClickListener {
-    void onItemClick(View view, int position);
-}
---
+++ common/src/main/java/io/liamju/comm/imageloader/ImageResizer.java
@@ -0,0 +1,16 @@
+package io.liamju.comm.imageloader;
+
+import android.graphics.Bitmap;
+
+import java.io.FileDescriptor;
+
+/**
+ * @author LiamJu
+ * @version 1.0
+ * @since 16/2/19
+ */
+public class ImageResizer {
+    public Bitmap decodeSampledBitmapFromFileDescriptor(FileDescriptor fileDescriptor, int reqWidth, int reqHeight) {
+        return null;
+    }
+}
